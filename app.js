11cfa9f459625f8d61d0e232f6539f492b1b16d6
Brian Lockerbie <brian.lockerbie@gmail.com>
2021-04-08 13:22:03 -0700

    Updated short version of showing sidebar

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click", function () {
   console.log(sidebar.classList);
-  if (sidebar.classList.contains("show-sidebar")) {
-    sidebar.classList.remove("show-sidebar");
-  } else{
-   sidebar.classList.add("show-sidebar");
-  }
+  // if (sidebar.classList.contains("show-sidebar")) {
+  //   sidebar.classList.remove("show-sidebar");
+  // } else{
+  //   sidebar.classList.add("show-sidebar");
+  // }
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function() {
+  sidebar.classList.remove("show-sidebar");
 });
